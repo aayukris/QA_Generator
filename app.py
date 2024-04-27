e254b92ac80e2388866f7b4c0cfba128642bd1ec
Aayush Krishna <72030892+aayukris@users.noreply.github.com>
2024-04-27 08:09:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,192 @@
+from flask import Flask, render_template, request
+import torch
+import requests
+import re
+import time
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+app = Flask(__name__)
+cleaned_answers = []
+cleaned_answers_f = []
+# Azure necessary things
+azure_endpoint = "https://sampleprac.cognitiveservices.azure.com/language/query-knowledgebases/projects/samplepracprac/sources?api-version=2023-04-01"
+url = "https://sampleprac.cognitiveservices.azure.com/language/authoring/query-knowledgebases/projects/samplepracprac/qnas?api-version=2023-04-01"
+headers = {
+    "Content-type": "application/json",
+    "Ocp-apim-subscription-key": "b5ed995c3bec44058f1f38c8db081331"
+}
+
+generated_qna = []
+distractors_str = []
+question = ""
+answer = ""
+# qa generation
+
+# t5_weights_path = '/Users/gopalkrishnan/Documents/t5-small-Race-QA-Generation-version0-step90000.pt'
+t5_weights_path = '/Users/gopalkrishnan/Documents/t5-base-Race-QA-Generation-version0-step30000.pt'
+t5_tokenizer = AutoTokenizer.from_pretrained("t5-base")
+t5_model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+t5_tokenizer.add_special_tokens({"sep_token": "<sep>"})
+t5_model.load_state_dict(torch.load(t5_weights_path, map_location=torch.device('cpu')))
+# t5_weights_path = '/Users/gopalkrishnan/Documents/t5-base'
+# t5_config_path = '/Users/gopalkrishnan/Documents/t5-base/config.json'
+# t5_model = T5ForConditionalGeneration.from_pretrained(t5_weights_path, config=t5_config_path)
+# t5_tokenizer = AutoTokenizer.from_pretrained(t5_weights_path)
+# t5_tokenizer.add_special_tokens({"sep_token": "<sep>"})
+# distractor generation
+distractor_weights_path = '/Users/gopalkrishnan/Documents/distractor'
+distractor_config_path = '/Users/gopalkrishnan/Documents/distractor/config.json'
+distractor_model = AutoModelForSeq2SeqLM.from_pretrained(distractor_weights_path, config=distractor_config_path)
+distractor_tokenizer = AutoTokenizer.from_pretrained(distractor_weights_path)
+distractor_tokenizer.add_special_tokens({"sep_token": "<sep>"})
+
+# Use regex to replace special characters between words with a space
+def remove_special_characters(text):
+    return re.sub(r'[^a-zA-Z0-9\s]', ' ', text)
+
+# Use regex to remove URLs and image tags
+def remove_urls_and_special_characters(text):
+    text_without_urls = re.sub(r'https?://\S+', '', text)
+    text_without_img_tags = re.sub(r'!\[Img\]', '', text_without_urls)
+    return remove_special_characters(text_without_img_tags)
+
+# insert URL
+def put_url(url):
+    body = [
+        {
+            "op": "add",
+            "value": {
+                "displayName": "source1",
+                "sourceUri": url,
+                "sourceKind": "url",
+                "source": url
+            }
+        }
+    ]
+
+    try:
+        # Post the URL to the portal
+        response_post = requests.patch(azure_endpoint, headers=headers, json=body)
+        if response_post.status_code == 202:
+            print("INSERT request successful.")
+            print(url)
+
+        else:
+            print(f"INSERT request failed with status code: {response_post.status_code}")
+            print("Response content:")
+            print(response_post.json())
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+# delete url
+def del_url(url):
+    body = [
+        {
+            "op": "delete",
+            "value": {
+                "displayName": "source1",
+                "sourceUri": url,
+                "sourceKind": "url",
+                "source": url
+            }
+        }
+    ]
+
+    try:
+        # Delete the URL to the portal
+        response_post = requests.patch(azure_endpoint, headers=headers, json=body)
+        if response_post.status_code == 202:
+            print("DELETE request successful.")
+            print(url)
+            time.sleep(30)
+
+        else:
+            print(f"DELETE request failed with status code: {response_post.status_code}")
+            print("Response content:")
+            print(response_post.json())
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+# get answer and generation
+def fetch_answers():
+    global cleaned_answers
+    global distractors_str
+    global cleaned_answers_f
+    global generated_qna
+    
+    # Reset variables
+    cleaned_answers = []
+    distractors_str = []
+    cleaned_answers_f = []
+    generated_qna = []
+    
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for entry in data.get('value', []):
+                answer = entry.get('answer')
+                if answer is not None:
+                    answer_cleaned = remove_urls_and_special_characters(answer)
+                    cleaned_answers.append(answer_cleaned)
+            combined_answers = []
+            context = ' '.join(cleaned_answers)
+            for i in range(0, len(cleaned_answers), 3):
+                group = cleaned_answers[i:i + 3]
+                combined_text = ' '.join(group)
+                combined_answers.append(combined_text)
+            non_empty_combined_answers = [answer for answer in combined_answers if answer.strip()]
+            non_empty_combined_answers = [phrase for phrase in non_empty_combined_answers if len(phrase.split()) > 50]
+            cleaned_answers_f.extend(non_empty_combined_answers)
+
+            # Generate QnA for each cleaned answer
+            generated_qna = []
+            distractors_dict = {}  # Keep track of distractors for each question
+            for cleaned_answer in cleaned_answers_f:
+                input_ids_t5 = t5_tokenizer.encode(cleaned_answer, return_tensors='pt')
+                output_ids_t5 = t5_model.generate(input_ids_t5, max_length=100)
+                question_answer = t5_tokenizer.decode(output_ids_t5[0], skip_special_tokens=False)
+                question_answer = question_answer.replace(t5_tokenizer.pad_token, "").replace(t5_tokenizer.eos_token, "")
+                question, answer = question_answer.split(t5_tokenizer.sep_token)
+                generated_qna.append({'question': question, 'answer': answer})
+                
+                # Generate distractors
+                input_text_distractor = " ".join([question, distractor_tokenizer.sep_token, answer, distractor_tokenizer.sep_token,context])
+                inputs_distractor = distractor_tokenizer(input_text_distractor, return_tensors="pt")
+                outputs_distractor = distractor_model.generate(**inputs_distractor, max_new_tokens=128)
+                distractors_list = distractor_tokenizer.decode(outputs_distractor[0], skip_special_tokens=False)
+                distractors_list = distractors_list.replace(distractor_tokenizer.pad_token, "").replace(distractor_tokenizer.eos_token, "")
+                distractors_list = [y.strip() for y in distractors_list.split(distractor_tokenizer.sep_token)]
+                print(distractors_list)
+                # Filter out duplicate distractors for each question
+                unique_distractors = list(set(distractors_list) - set([answer]))
+                distractors_dict[question] = unique_distractors
+                print(unique_distractors)
+            
+            # Create the final distractors_str list
+            for question_answer in generated_qna:
+                question_text = question_answer['question']
+                distractors_str.append({'qna': {'question': question_text, 'answer': question_answer['answer']},
+                                       'distractors': distractors_dict.get(question_text, [])})
+        else:
+            print(f"GET request failed with status code: {response.status_code}")
+            print("Response content:")
+            print(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        put_url(url)
+        time.sleep(60)
+        fetch_answers()
+        del_url(url)
+    return render_template('index.html', cleaned_answers=cleaned_answers_f, generated_qna=generated_qna,
+                           distractors=distractors_str)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ metrics_calculate.py
@@ -0,0 +1,109 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+
+t5_weights_path = '/Users/gopalkrishnan/Documents/t5-base-Race-QA-Generation-version0-step30000.pt'
+t5_tokenizer = AutoTokenizer.from_pretrained("t5-base")
+t5_model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+t5_tokenizer.add_special_tokens({"sep_token": "<sep>"})
+t5_model.load_state_dict(torch.load(t5_weights_path, map_location=torch.device('cpu')))
+
+import torch
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from datasets import load_dataset
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from bert_score import score as bert_score
+from rouge_score import rouge_scorer
+
+# Load SQuAD dataset
+# squad = load_dataset("squad")
+triviaqa = load_dataset('trivia_qa', 'rc')
+
+
+# Load T5 model and tokenizer
+
+# Extract unique contexts from the first 2000 examples in the SQuAD dataset
+# unique_contexts = set(example["entity_pages"]["wiki_context"] for example in triviaqa["train"])
+unique_contexts = set(example["entity_pages"]["wiki_context"] if not isinstance(example["entity_pages"]["wiki_context"], list) else ' '.join(example["entity_pages"]["wiki_context"]) for example in triviaqa["train"])
+
+# Function to generate questions for a given context
+def generate_question(context):
+    inputs = t5_tokenizer(context, return_tensors="pt")
+    outputs = t5_model.generate(**inputs, max_length=100)
+    question_answer = t5_tokenizer.decode(outputs[0], skip_special_tokens=False)
+    question_answer = question_answer.replace(t5_tokenizer.pad_token, "").replace(t5_tokenizer.eos_token, "")
+    # question, answer = question_answer.split(t5_tokenizer.sep_token)
+    # return question
+    try:
+        question, answer = question_answer.split(t5_tokenizer.sep_token)
+        return question
+    except ValueError:
+        print(f"Skipping context due to split error: {context}")
+        return None
+
+# Function to calculate cosine similarity between two strings
+def calculate_similarity(str1, str2):
+    vectorizer = TfidfVectorizer()
+    vectors = vectorizer.fit_transform([str1, str2])
+    similarity = cosine_similarity(vectors)
+    return similarity[0, 1]
+
+# Initialize lists to store generated questions and most similar questions
+generated_questions = []
+similar_squad_questions = []
+bert_scores = []
+rouge_scores = []
+
+# Instantiate ROUGE scorer
+scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+# Iterate through each unique context and generate questions
+i=0
+counter = 0
+for context in unique_contexts:
+    i+=1
+    # Generate question for the context
+    generated_question = generate_question(context)
+
+    if generated_question is None:
+        continue
+
+    # Filter SQuAD dataset for the same context
+    similar_examples = [example for example in triviaqa["train"] if example["entity_pages"]["wiki_context"] == context]
+
+    # Find the most similar question from the SQuAD dataset
+    most_similar_example = max(similar_examples, key=lambda x: calculate_similarity(x["question"], generated_question))
+
+    # Add generated question and most similar SQuAD question to lists
+    generated_questions.append(generated_question)
+    similar_squad_questions.append(most_similar_example["question"])
+    _, _, bert_f1 = bert_score([generated_question], [most_similar_example["question"]], lang="en", model_type="bert-base-uncased")
+    bert_scores.append(bert_f1.item())
+
+    # Calculate ROUGE score
+    rouge_score = scorer.score(generated_question, most_similar_example["question"])['rougeL'].fmeasure
+    rouge_scores.append(rouge_score)
+
+    print(i)
+    counter += 1
+
+    # If counter reaches 100, calculate metrics and reset lists
+    if counter == 200:
+        # Calculate and print metrics
+        # You can calculate BLEU score here if needed
+        print("Metrics after processing " + str(i) + " contexts:")
+        print(f"Average BERT Score: {sum(bert_scores) / len(bert_scores)}")
+        print(f"Average ROUGE Score: {sum(rouge_scores) / len(rouge_scores)}\n")
+        bleu_1 = corpus_bleu([[question] for question in similar_squad_questions], generated_questions, weights=(1, 0, 0, 0), smoothing_function=SmoothingFunction().method1)
+        bleu_2 = corpus_bleu([[question] for question in similar_squad_questions], generated_questions, weights=(0.5, 0.5, 0, 0), smoothing_function=SmoothingFunction().method1)
+        bleu_3 = corpus_bleu([[question] for question in similar_squad_questions], generated_questions, weights=(0.33, 0.33, 0.33, 0), smoothing_function=SmoothingFunction().method1)
+        bleu_4 = corpus_bleu([[question] for question in similar_squad_questions], generated_questions, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=SmoothingFunction().method1)
+
+        print(f"BLEU-1 Score: {bleu_1}")
+        print(f"BLEU-2 Score: {bleu_2}")
+        print(f"BLEU-3 Score: {bleu_3}")
+        print(f"BLEU-4 Score: {bleu_4}")
+        
+        bleu_score = corpus_bleu([[question] for question in similar_squad_questions], generated_questions)
+        print(f"BLEU Score: {bleu_score}")
+        counter = 0
